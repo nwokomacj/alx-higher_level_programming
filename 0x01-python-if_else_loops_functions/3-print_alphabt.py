cd3f5dbca3187ec860d70348ee0b19335e5da540
nwokomacj <nwokomachidiebube123@gmail.com>
2022-12-03 13:11:05 -0800

    Print all the letters except q and e

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -2,5 +2,5 @@
 """print the alphabet in lowercase, not followed by a new line."""
 
 for let in range(97, 123):
-  if chr(let) != 'q' and chr(let) != 'e':
-    print("{}" .format(chr(let)), end="")
+    if chr(let) != 'q' and chr(let) != 'e':
+        print("{}" .format(chr(let)), end="")
