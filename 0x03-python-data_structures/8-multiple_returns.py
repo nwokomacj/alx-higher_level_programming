18b17b648acd1486116daf351235f51f7b49f7af
nwokomacj <nwokomachidiebube123@gmail.com>
2022-12-06 18:11:38 -0800

    a function that returns a tuple with the length of a string and its first character.

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def multiple_returns(sentence):
+
+    multup = ()
+    if len(sentence) == 0:
+        multup[0] == "None"
+    else:
+        multup = len(sentence), sentence[0]
+        return multup
