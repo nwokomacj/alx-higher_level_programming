7d54bff9f9438565c4bf30ff14f48a132839ae3c
nwokomacj <nwokomachidiebube123@gmail.com>
2022-12-07 20:48:29 -0800

    updated

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,21 +1,15 @@
 #!/usr/bin/python3
 
-'''
-function that replaces all occurrences of an element by another in a new list.
 
-@my_list: is the initial list
-@search: is the element to replace in the list
-@replace: is the new element
-
-Return: the new list
-
-'''
 def search_replace(my_list, search, replace):
-    
+    """
+    A function that replaces all occurrences
+    of an element by another in a new list
+    """
     new_list = []
-    for i in my_list:
-        if i == search:
+    for element in my_list:
+        if element == search:
             new_list.append(replace)
         else:
-            new_list.append(i)
+            new_list.append(element)
     return new_list
--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-# function that adds all unique integers in a list (only once for each integer).
+
 
 def uniq_add(my_list=[]):
-
-    # new_list = []
-    new_list = set(my_list)
+    """
+    A function that adds all unique
+    integers in a list (only once for each integer)
+    """
+    new_list = []
     sum = 0
-    for i in new_list:
-      #  if i not in new_list:
-         sum += i
-     #   new_list.append(i)
+    for num in my_list:
+        if num not in new_list:
+            sum += num
+            new_list.append(num)
     return sum
