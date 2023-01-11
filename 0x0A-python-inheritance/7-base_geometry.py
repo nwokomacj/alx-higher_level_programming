234f30f552c7485ec659517b4eab4259bfdd5eec
nwokomacj <nwokomachidiebube123@gmail.com>
2023-01-10 17:37:57 -0800

    a class MyInt that inherits from int:MyInt is a rebel. MyInt has == and != operators inverted

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Defines a base geometry class BaseGeometry"""
+
+
+def area(self):
+    """not implemented"""
+    raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validates a parameter as an integer
+
+        Args:
+            name (str): The name of the parameter
+            value (int): The parameter to validate
+        Raises:
+            TypeError: if value is not an integer
+            ValueError: if value is <= 0
+        """
+
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+             raise ValueError("{} must be greater than 0".format(name))
